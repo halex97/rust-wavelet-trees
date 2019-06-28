6777654f66fae1eceff903a09af719daa8f776cf
Alexander Hahn <43539214+halex97@users.noreply.github.com>
2019-06-28 11:32:22 +0200

    Change signature of GraphWaveletTree and remove labels. Add LabelledGraphWaveletTree signature.

--- src/graph.rs
+++ src/graph.rs
@@ -2,29 +2,36 @@ use bio::data_structures::rank_select::RankSelect;
 use bv::BitVec;
 use petgraph::graph::Graph;
 
-pub struct WaveletGraph<T: PartialOrd + Clone> {
-    lists: WaveletTree<T>,
-    separator: RankSelect
+pub struct LabelledGraphWaveletTree {
+    graphTree: WaveletGraph,
+    labels: Vec<T>
 }
 
-impl <T: PartialOrd + Clone> WaveletGraph<T> {
+pub struct GraphWaveletTree {
+    /// The wavelet tree represents the concatenated adjacency lists of the graph
+    tree: WaveletTree<u64>,
+    /// The bitmap marks the beginnings of the adjacency lists
+    bitmap: RankSelect
+}
+
+impl GraphWaveletTree {
     pub fn from_graph(graph: Graph) -> Self {
         unimplemented!();
     }
 
-    pub fn from_adjacency_list(nodes: &[T], edges: &[T, T]) -> Self {
+    pub fn from_adjacency_lists(nodes: NodeIndices, lists: Vec<Neighbours>) -> Self {
         unimplemented!();
     }
 
-    pub fn predecessor(&self, index: &T) -> Vec<T> {
+    pub fn predecessor(&self, index: u64) -> u64 {
         unimplemented!();
     }
 
-    pub fn successor(&self, index: &T) -> Vec<T> {
+    pub fn successor(&self, index: u64) -> u64 {
         unimplemented!();
     }
 
-    pub fn edge_exists(&self, predecessor: &T, successor: &T) -> bool {
+    pub fn edge_exists(&self, predecessor: u64, successor: u64) -> bool {
         unimplemented!();
     }
 
