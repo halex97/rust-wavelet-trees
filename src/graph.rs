bbb524ed1d966e7e181e84747497ef6c43755774
Alexander Hahn <43539214+halex97@users.noreply.github.com>
2019-07-03 16:26:52 +0200

    Implement from_graph and from_adjacency_lists

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,12 @@
 use bio::data_structures::rank_select::RankSelect;
-use bv::BitVec;
-use petgraph::graph::Graph;
+use bv::{BitVec, BitsMut};
+use petgraph::EdgeType;
+use petgraph::graph::{Graph, NodeIndices, NodeIndex, Neighbors, DefaultIx, IndexType};
 use serde::{Serialize, Deserialize};
+
+use crate::WaveletTree;
+use crate::pointerless::PointerlessWaveletTree;
+use core::borrow::BorrowMut;
 
 #[derive(Serialize, Deserialize)]
 pub struct LabelledGraphWaveletTree<T> {
@@ -11,19 +16,53 @@ pub struct LabelledGraphWaveletTree<T> {
 
 #[derive(Serialize, Deserialize)]
 pub struct GraphWaveletTree {
-    /// The wavelet tree represents the concatenated adjacency lists of the graph
-    tree: WaveletTree<usize>,
+    /// The wavelet tree represents the concatenated adjacency lists of the graph. The node indices
+    /// are stored as usize integers.
+    tree: PointerlessWaveletTree<usize>,
     /// The bitmap marks the beginnings of the adjacency lists
     bitmap: RankSelect
 }
 
 impl GraphWaveletTree {
-    pub fn from_graph(graph: Graph) -> Self {
-        unimplemented!();
+
+    pub fn from_graph<N,E,Ty:EdgeType,Ix:IndexType> (graph: Graph<N,E,Ty,Ix>) -> Self {
+        let nodes = graph.node_indices();
+
+        let mut lists = Vec::with_capacity(nodes.len());
+
+        for node in nodes {
+            lists.push(graph.neighbors(node));
+        }
+
+        Self::from_adjacency_lists(lists)
     }
 
-    pub fn from_adjacency_lists(nodes: NodeIndices, lists: Vec<Neighbours>) -> Self {
-        unimplemented!();
+    pub fn from_adjacency_lists<E,Ix:IndexType> (lists: Vec<Neighbors<E, Ix>>) -> Self {
+        // Build the sequence (which is the concatenation of the adjacency lists) and the
+        // corresponding bit vector marking the beginnings of the individual lists in the sequence.
+        let mut sequence : Vec<usize> = Vec::new();
+        let mut lists_bv : BitVec<u8> = BitVec::new();
+
+        // Iterate over all adjacency lists
+        for neighbors in lists {
+            // A '1' in the bit vector separates two adjacency lists.
+            lists_bv.push(true);
+
+            // Iterate over all neighbors (i.e. adjacent nodes that appear in the list)
+            for n in neighbors {
+                // Add the neighbor's index to the sequence
+                sequence.push(n.index());
+
+                // '0's in the bit vector represent node indices in the adjacency lists
+                lists_bv.push(false);
+            }
+        }
+
+        // Return a pointerless wavelet tree representing the sequence together with the bitmap.
+        GraphWaveletTree {
+            tree: PointerlessWaveletTree::from_slice(sequence.as_slice()),
+            bitmap: RankSelect::new(lists_bv, 1)
+        }
     }
 
     pub fn predecessor(&self, index: usize) -> NodeIndex {
@@ -47,6 +86,8 @@ mod tests {
     use super::super::*;
 
     #[test]
-
+    fn test_from_graph() {
+        unimplemented!();
+    }
 
 }
