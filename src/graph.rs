9511e747371f8c1dcd809397b4459db93885a846
Alexander Hahn <43539214+halex97@users.noreply.github.com>
2019-07-03 18:33:50 +0200

    Implement method access_reverse_neighbor

--- src/graph.rs
+++ src/graph.rs
@@ -83,6 +83,21 @@ impl GraphWaveletTree {
         n.and_then(|x| self.tree.access(x)).map(|x| x.clone())
     }
 
+    /// Access the i-th reverse neighbor (i.e. predecessor) of node v
+    pub fn access_reverse_neighbor(&self, v: usize, i: usize) -> Option<usize> {
+        // First we need to find the i-th occurrence of v in the sequence of concatenated adjacency
+        // lists, which is represented by the wavelet tree.
+        let p = self.tree.select(&v, i as u64);
+
+        // Then we need to find the p-th occurence of a '0' in the bitmap. This corresponds ot the
+        // position of v in the bitmap representing the concatenated adjacency lists.
+        let p0 = p.and_then(|x| self.bitmap.select_0(x));
+
+        // Now we can count the number of '1's up until position p0 in order to find out which
+        // adjacency list the i-th occurence of v is in.
+        p0.map(|x| self.bitmap.rank_1(x) as usize)
+    }
+
     pub fn predecessor(&self, index: usize) -> NodeIndex {
         unimplemented!();
     }
